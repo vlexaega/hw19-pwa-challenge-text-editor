ef727d7529e802eaa216bfefcda3e65e8490133f
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-07-20 21:35:25 -0400

    cleans up the src-sw file

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,47 +26,6 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-// Register route for caching dynamic CSS and JS files.
-// i.e. bootstrap, jQuery, ...
-// The StaleWhileRevalidate strategy serves content from cache AND loads it from source if needed.
-// registerRoute(
-//   ({ request }) => {
-//     console.log(request);
-//     return (
-//       // CSS
-//       request.destination === 'style' ||
-//       // JavaScript
-//       request.destination === 'script'
-//     );
-//   },
-//   new StaleWhileRevalidate({
-//     cacheName: 'static-resources',
-//     plugins: [
-//       new CacheableResponsePlugin({
-//         statuses: [0, 200],
-//       }),
-//     ],
-//   })
-// );
-
-// Register route for caching dynamic images
-// The cache first strategy is often the best choice for images because it saves bandwidth and improves performance.
-// registerRoute(
-//   ({ request }) => request.destination === 'image',
-//   new CacheFirst({
-//     cacheName: 'my-image-cache',
-//     plugins: [
-//       new CacheableResponsePlugin({
-//         statuses: [0, 200],
-//       }),
-//       new ExpirationPlugin({
-//         maxEntries: 60,
-//         maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-//       }),
-//     ],
-//   })
-// );
-
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // Set up asset cache
